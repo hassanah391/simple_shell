4b663eb02ff2d7de4e131e4b1e3585b81bae8ff0
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-08-31 03:37:43 +0300

    Try to handle memory leak

--- non_interactive.c
+++ non_interactive.c
@@ -1,4 +1,24 @@
 #include "shell.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * is_whitespace - Check if a string contains only whitespace
+ * @str: The string to check
+ *
+ * Return: 1 if string is only whitespace, 0 otherwise
+ */
+int is_whitespace(const char *str)
+{
+    while (*str)
+    {
+        if (!isspace((unsigned char)*str))
+            return 0;
+        str++;
+    }
+    return 1;
+}
+
 /**
 * non_interactive - handle inputs from non-interactive mode
 *   (pipe lines or script file) (EX: echo "/bin/ls" | ./hsh)
@@ -8,49 +28,53 @@
 */
 int non_interactive(char *programname)
 {
-	char *inputline = NULL, *argv[10], *full_path;
-	int number_command;
-	size_t len = 0;
-	ssize_t read;
+    char *inputline = NULL, *argv[10] = {NULL}, *full_path;
+    int number_command;
+    size_t len = 0;
+    ssize_t read;
 
-	number_command = 0;
+    number_command = 0;
 
-	while ((read = getline(&inputline, &len, stdin)) != -1)
-	{
-		if (read > 0 && inputline[read - 1] == '\n')
-			inputline[read - 1] = '\0';  /* Remove trailing newline */
+    while ((read = getline(&inputline, &len, stdin)) != -1)
+    {
+        if (read > 0 && inputline[read - 1] == '\n')
+            inputline[read - 1] = '\0';  /* Remove trailing newline */
 
-		number_command++;
+        if (is_whitespace(inputline))  /* Skip lines that are only whitespace */
+            continue;
 
-		arguments(inputline, argv); /* handle arguments and store it in argv */
-		if (argv[0] == NULL) /* Skip empty lines */
-			continue;
+        number_command++;
 
-		if (strcmp(argv[0], "exit") == 0) /* if user input "exit" terminate shell */
-		{
-			free(inputline);
-			exit(EXIT_SUCCESS);
-		}
-		if (strcmp(argv[0], "env") == 0)
-		{
-			_env();
-		}
-		else
-		{
-			full_path = handle_path_var(argv[0]);
-			if (full_path == NULL)
-			{
-				fprintf(stderr, "%s: %d: %s: not found\n",
-						programname, number_command, argv[0]);
-			}
-			else
-			{
-				excute_command(full_path, argv);
-				free(full_path);
-			}
-		}
-	}
+        arguments(inputline, argv); /* handle arguments and store it in argv */
+        if (argv[0] == NULL) /* Skip empty lines */
+            continue;
 
-	free(inputline);
-	return (0);
+        if (strcmp(argv[0], "exit") == 0) /* if user input "exit" terminate shell */
+        {
+            free(inputline);
+            exit(EXIT_SUCCESS);
+        }
+        else if (strcmp(argv[0], "env") == 0)
+        {
+            _env();
+        }
+        else
+        {
+            full_path = handle_path_var(argv[0]);
+            if (full_path == NULL)
+            {
+                fprintf(stderr, "%s: %d: %s: not found\n",
+                        programname, number_command, argv[0]);
+            }
+            else
+            {
+                excute_command(full_path, argv);
+                free(full_path);
+            }
+        }
+        memset(argv, 0, sizeof(argv));
+    }
+
+    free(inputline);
+    return (0);
 }
